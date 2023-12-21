231e7b1a5a52e6252d318c1b931c269630f21d88
Harold-coder <hjc2154@columbia.edu>
2023-12-22 00:04:39 +0100

    Connected inventory and made it more sophisticated

--- lambda_function.py
+++ lambda_function.py
@@ -1,44 +1,40 @@
 import requests
 import asyncio
 import aiohttp
-import time
 
-async def fetch(session, resource):
-    url = resource["url"]
+async def fetch(session, url):
     async with session.get(url) as response:
-        return {
-            "resource": resource["resource"],
-            "data": await response.text()  # Assuming the response is text; modify if it's JSON or other format
-        }
+        return await response.json()  # Assuming the response is JSON
 
-async def get_data_async(resources):
+async def get_data_async(url):
     async with aiohttp.ClientSession() as session:
-        tasks = [asyncio.ensure_future(fetch(session, res)) for res in resources]
-        responses = await asyncio.gather(*tasks)
-        return responses
+        return await fetch(session, url)
 
-def get_data_sync(resources):
-    full_result = {}
-    for r in resources:
-        response = requests.get(r["url"])
-        full_result[r["resource"]] = response.text  # Assuming the response is text
-    return full_result
+def get_data_sync(url):
+    response = requests.get(url)
+    return response.json()  # Assuming JSON responses
 
 def lambda_handler(event, context):
-    # Whatsup
-    # Define the resources
-    resources = [
-        {
-            "resource": "manage_orders",
-            "url": 'https://lion-leftovers-om.ue.r.appspot.com/manage_orders'
-        },
-        # Add other resources here if needed
-    ]
+    # Extract the resource name and parameters from the event
+    resource_name = event.get('resource_name')
+    custom_id = event.get('custom_id', '')
 
-    # Call the synchronous or asynchronous function based on the Lambda event
-    # The False in .get('async', False) is a default value that will be used if the async key is not found in the event.
-    if event.get('async', False):
-        return asyncio.run(get_data_async(resources))
+    # Define base URLs for inventory and order management
+    inventory_base_url = 'http://ec2-18-222-146-24.us-east-2.compute.amazonaws.com:8012/'
+    order_base_url = 'https://lion-leftovers-om.ue.r.appspot.com/'
+
+    # Construct the URL based on the resource name
+    if resource_name == 'manage_orders':
+        url = f'{order_base_url}{resource_name}'
+    elif resource_name in ['available_meals', 'manage_inventory']:
+        url = f'{inventory_base_url}{resource_name}'
+    elif resource_name in ['meals_by_dining_hall', 'inventory_item'] and custom_id:
+        url = f'{inventory_base_url}{resource_name}/{custom_id}'
     else:
-        return get_data_sync(resources)
+        return {"error": "Invalid resource name or missing custom_id for the requested resource"}
 
+    # Determine if the request should be asynchronous or synchronous
+    if event.get('async', False):
+        return asyncio.run(get_data_async(url))
+    else:
+        return get_data_sync(url)
